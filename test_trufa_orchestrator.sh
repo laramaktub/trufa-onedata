82f5fea1bba2195500f6a3fec6c4ed84a7b247cf
laramaktub <lara.cern@gmail.com>
2017-09-08 12:39:38 +0200

    reduced version for deployment only

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -6,46 +6,9 @@ apt-get update && apt-get install -y ansible && rm -rf /var/lib/apt/lists/*
 ansible-galaxy install indigo-dc.oneclient && ansible-playbook /etc/ansible/roles/indigo-dc.oneclient/tests/test.yml
 
 env
-echo 'ich bin hier'
-mkdir -p /onedata/input
-mkdir -p /onedata/output
+
+mkdir -p /onedata/TRUFA
 
 ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/input || exit 1
-ONECLIENT_AUTHORIZATION_TOKEN="$OUTPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$OUTPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
-
-cd /onedata/input
-
-
-INPUTDIR="/onedata/input/$INPUT_PATH"
-OUTPUTDIR=
-
-mkdir -p "$OUTPUTDIR"
-
-OUT_FOLDER="/onedata/output/TRUFA/output"
-IN_FOLDER="/onedata/input/$INPUT_PATH"
-STAT_FOLDER="/onedata/output/$OUTPUT_PATH"
-READS_FILES="$IN_FOLDER/reads_left.fq $IN_FOLDER/reads_right.fq"
-
-echo 'Hello'
-echo $OUT_FOLDER
-echo $READS_FILES
-
-mkdir -p "$OUT_FOLDER"
-
-
-mkdir ${STAT_FOLDER}fastqc_report
-
-echo `date +%F\ %H:%M:%S` "START FastQC" >> ${OUT_FOLDER}.LOG.txt
-
-reads_in=($READS_FILES)
-
-for ((i=0; i<${#reads_in[@]}; i++)); do
-
-    fastqc -t 2  --noextract ${reads_in[ i ]} -o ${STAT_FOLDER}fastqc_report
-    echo FASTQC: IN: ${reads_in[ i ]} >> ${OUT_FOLDER}.LOG.txt
-done
-wait
-ls ${OUT_FOLDER}
-echo `date +%F\ %H:%M:%S` "END FastQC" >> ${OUT_FOLDER}.LOG.txt
 
 
