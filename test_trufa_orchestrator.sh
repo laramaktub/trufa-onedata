22a069da1d6f0174243e31c76c51c4839732596c
laramaktub <lara.cern@gmail.com>
2017-09-11 12:47:25 +0200

    lala

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update -y
 apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
+apt-add-repository -y ppa:ansible/ansible
 apt-get update && apt-get install -y ansible && rm -rf /var/lib/apt/lists/*
 ansible-galaxy install indigo-dc.oneclient && ansible-playbook /etc/ansible/roles/indigo-dc.oneclient/tests/test.yml
 
@@ -9,4 +9,10 @@ env
 
 mkdir -p /onedata/TRUFA
 
-ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/TRUFA || exit 1
+export ONE_CLIENT_AUTHORIZATION_TOKEN=MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzYmlkZW500aWZpZXIgVlY4UHBIQXBrV001aay1RcDYzTmdNM1RYVnJDM1FYNWtYVmp2ZmVJeVdlSQowMDFhY2lkIHRpbWUgPCAxNTM2MTM3ODg3CjAwMmZzaWduYXR1cmUgegSVJbLV02mOP7NEkKURNGfj2BJLKEhgXnGhRMl026kJ4K
+export PROVIDER_HOSTNAME=oneprovider-test.cloud.ba.infn.it
+
+
+oneclient --authentication token --no_check_certificate  /onedata/TRUFA
+
+
