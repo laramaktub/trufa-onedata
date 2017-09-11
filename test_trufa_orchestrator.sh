2ea6cc9605e30ba030add79b4a5b6b7ee17f889d
laramaktub <lara.cern@gmail.com>
2017-09-11 11:47:09 +0200

    lla

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 env
 mkdir -p /onedata/TRUFA
-
+apt-get update -y
+apt-get install software-properties-common -y 
+apt-add-repository ppa:ansible/ansible
  apt-get update -y
- apt-get install software-properties-common -y
- apt-add-repository ppa:ansible/ansible
- apt-get update -y
- apt-get install -y ansible
- rm -rf /var/lib/apt/lists/*
+ apt-get install -y
+ ansible rm -rf /var/lib/apt/lists/*
  ansible-galaxy install indigo-dc.oneclient
  ansible-playbook /etc/ansible/roles/indigo-dc.oneclient/tests/test.yml
-
 
 ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/TRUFA || exit 1
