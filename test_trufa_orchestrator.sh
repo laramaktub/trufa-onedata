384e27d19ac7d25b02ace03520a7a30524556226
laramaktub <lara.cern@gmail.com>
2017-10-30 12:54:32 +0100

    lala

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -7,7 +7,7 @@ mkdir -p /onedata/TRUFA
 
 ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token  -o rw /onedata/TRUFA || exit 1
 
-echo " esta ya aqui "
+echo " esta ya aquiiiiiii y funcionaaa!!! "
 #cd /onedata/TRUFA/TRUFA/users/admin/jobs/Job_trial_new_release2/.cache/tmp/
 #chmod 777 script_fastqc_*
 #/bin/bash script_fastqc_* 
